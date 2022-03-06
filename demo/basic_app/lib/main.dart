4d16da8aa1c50004af1c8732998f19009561bfd7
Dat <datndd2604@gmail.com>
2022-03-06 23:30:13 +0700

    update container

--- demo/basic_app/lib/main.dart
+++ demo/basic_app/lib/main.dart
@@ -13,15 +13,43 @@ class MyApp extends StatelessWidget {
     var stars = Row();
 
     return MaterialApp(
-        home: Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Icon(Icons.star, color: Colors.green[500]),
-        Icon(Icons.star, color: Colors.green[500]),
-        Icon(Icons.star, color: Colors.green[500]),
-        const Icon(Icons.star, color: Colors.black),
-        const Icon(Icons.star, color: Colors.black),
-      ],
-    ));
+        title: 'Flutter Demo',
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Flutter layout demo'),
+          ),
+          body: Center(
+            child: _buildImageColumn(),
+          ),
+        ));
   }
+
+  Widget _buildImageColumn() {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.black26,
+      ),
+      child: Column(
+        children: [_buildImagesRow(1), _buildImagesRow(2), _buildImagesRow(3)],
+      ),
+    );
+  }
+
+  Widget _buildImagesRow(int imageIndex) => Row(
+        children: [
+          _buildDecoratedImage(imageIndex),
+          _buildDecoratedImage(imageIndex)
+        ],
+      );
+
+  Widget _buildDecoratedImage(int imageIndex) => Expanded(
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(width: 10, color: Colors.black38),
+            borderRadius: const BorderRadius.all(Radius.circular(8)),
+          ),
+          margin: const EdgeInsets.all(4),
+          child: Image.asset('images/pic$imageIndex.jpg'),
+        ),
+      );
 }
