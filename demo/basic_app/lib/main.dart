610d5565e104ef6aa00c9c4b0fb5802bd1123fd6
Dat <datndd2604@gmail.com>
2022-03-07 04:01:31 +0700

    update ListView

--- demo/basic_app/lib/main.dart
+++ demo/basic_app/lib/main.dart
@@ -19,37 +19,41 @@ class MyApp extends StatelessWidget {
             title: const Text('Flutter layout demo'),
           ),
           body: Center(
-            child: _buildImageColumn(),
+            child: _buildList(),
           ),
         ));
   }
 
-  Widget _buildImageColumn() {
-    return Container(
-      decoration: const BoxDecoration(
-        color: Colors.black26,
-      ),
-      child: Column(
-        children: [_buildImagesRow(1), _buildImagesRow(2), _buildImagesRow(3)],
-      ),
+  Widget _buildList() {
+    return ListView(
+      children: [
+        _tile('CineArts at the Empire', '85 W Portal Ave', Icons.theaters),
+        _tile('The Castro Theater', '429 Castro St', Icons.theaters),
+        _tile('Alamo Drafthouse Cinema', '2550 Mission St', Icons.theaters),
+        _tile('Roxie Theater', '3117 16th St', Icons.theaters),
+        _tile('United Artists Stonestown Twin', '501 Buckingham Way',
+            Icons.theaters),
+        _tile('AMC Metreon 16', '135 4th St #3000', Icons.theaters),
+        const Divider(),
+        _tile('K\'s Kitchen', '757 Monterey Blvd', Icons.restaurant),
+        _tile('Emmy\'s Restaurant', '1923 Ocean Ave', Icons.restaurant),
+        _tile('Chaiya Thai Restaurant', '272 Claremont Blvd', Icons.restaurant),
+        _tile('La Ciccia', '291 30th St', Icons.restaurant),
+      ],
     );
   }
 
-  Widget _buildImagesRow(int imageIndex) => Row(
-        children: [
-          _buildDecoratedImage(imageIndex),
-          _buildDecoratedImage(imageIndex)
-        ],
-      );
-
-  Widget _buildDecoratedImage(int imageIndex) => Expanded(
-        child: Container(
-          decoration: BoxDecoration(
-            border: Border.all(width: 10, color: Colors.black38),
-            borderRadius: const BorderRadius.all(Radius.circular(8)),
-          ),
-          margin: const EdgeInsets.all(4),
-          child: Image.asset('images/pic$imageIndex.jpg'),
-        ),
-      );
+  ListTile _tile(String title, String subtitle, IconData icon) {
+    return ListTile(
+      title: Text(
+        title,
+        style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 20),
+      ),
+      subtitle: Text(subtitle),
+      leading: Icon(
+        icon,
+        color: Colors.blue[500],
+      ),
+    );
+  }
 }
